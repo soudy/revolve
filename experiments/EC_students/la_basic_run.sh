7771a67c7e10d33684fb86a4762b71094c53a398
Steven Oud <soud@pm.me>
2021-09-27 17:45:01 +0200

    Robo reptiles first experiment scripts

---
+++ experiments/EC_students/la_basic_run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -u
+
+N=20
+EXPERIMENT_NAME="la_basic"
+CORES=4
+MANAGER_PATH="experiments/EC_students/la_basic.py"
+NO_LA="${1:-}"
+
+set -x
+
+cd ../../
+
+for i in $(seq 1 $N); do
+  echo "---------------- RUN $i/$N -----------------"
+
+  if [[ "$NO_LA" = "nola" ]]; then
+    echo "!! LA disabled !!"
+    export NO_LA
+  fi
+
+  ./revolve.py \
+    --simulator-cmd gazebo \
+    --run $i \
+    --experiment-name $EXPERIMENT_NAME \
+    --manager $MANAGER_PATH \
+    --n-cores $CORES
+done

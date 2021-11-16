fc1aa1b383fa5dc09d5793a6a1b10ee9da97086a
Steven Oud <soud@pm.me>
2021-11-16 12:43:49 +0100

    Update hills run script

--- experiments/EC_students/la_hills_run.sh
+++ experiments/EC_students/la_hills_run.sh
@@ -19,11 +19,11 @@ for i in $(seq 1 $N); do
 
   if [[ "$NO_LA" = "nola" ]]; then
     EXPERIMENT_NAME="hills_nola_30deg"
-    PORT_START=3333
+    PORT_START=9999
     echo "!! LA disabled !!"
   else
     EXPERIMENT_NAME="hills_la_30deg"
-    PORT_START=4444
+    PORT_START=8888
     echo "!! LA enabled !!"
   fi
 
